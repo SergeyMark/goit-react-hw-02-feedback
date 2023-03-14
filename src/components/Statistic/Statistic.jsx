33f2b1d46287a2f0c8f1fe7b9a900567ca97a0fb
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-03-14 22:27:22 +0200

    Update Statistic.jsx

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 export const Statistic = ({good, neutral, bad, total, percent}) => {
     return (
         <ul>
-            <li>good{good}</li>
-            <li>neutral{neutral}</li>
-            <li>bad{bad}</li>
-            <li>total{total}</li>
-            <li>Positive feedback:{percent}</li>
+            <li>good {good}</li>
+            <li>neutral {neutral}</li>
+            <li>bad {bad}</li>
+            <li>total {total}</li>
+            <li>Positive feedback: {percent}</li>
         </ul>
     )
 }
